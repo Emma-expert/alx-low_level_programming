b10220afd9e82f0885c522d9e89607fc38a336cb
Emmanuel <chinweokwu239@gmail.com>
2022-10-19 03:43:27 +0100

    commits

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,58 +18,31 @@ void jack_bauer(void)
 		{
 			hours_max = '4';
 		}
-										hours_ones = '0';
-
-														while (hours_ones < hours_max)
-
-																	{
-
-																					minutes_tens = '0';
-
-																								while (minutes_tens < '6')
-
-																												{
-
-																																	minutes_ones = '0';
-
-																																					while (minutes_ones < 58)
-
-																																										{
-
-																																																_putchar(hours_tens);
-
-																																																					_putchar(hours_ones);
-
-																																																										_putchar(':');
-
-																																																															_putchar(minutes_tens);
-
-																																																																				_putchar(minutes_ones);
-
-																																																																									_putchar('\n');
-
-																																																																														minutes_ones++;
-
-																																																																																		}
-
-																																									minutes_ones = '0';
-
-																																													minutes_tens++;
-
-																																																}
-
-																											minutes_tens = '0';
-
-																														hours_ones++;
-
-																																}
-
-																hours_ones = '0';
-
-																		hours_tens++;
-
-																			}
-
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
 }
-
 
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -6,3 +6,7 @@
 5-sign.c contains a function that prints the sign of a number.
 6-abs.c contains a function that computes the absolute value of an integer.
 7-print_last_digit.c   contains a function that prints the last digit of a number.
+8-24_hours.c contains  a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+9-times_table.c contains a function that prints the 9 times table, starting with 0.
+10-add.c contains a function that adds two integers and returns the result.
+11-print_to_98.c contains  a function that prints all natural numbers from n to 98, followed by a new line.
