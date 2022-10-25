3ab8fe15a6ac415a1a6456898caff2799afabf4a
Emma-expert <chinweokwu239@gmial.com>
2022-10-24 17:44:08 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ * reset_to_98 - resets the number to 98
+ * @n : input
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains the codes for pointers, arrays and string in C
